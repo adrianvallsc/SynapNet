4db86b87747c4c37a643d25749d273b7de1dee76
adrianvallsc <adrianvallsc@gmail.com>
2024-06-11 22:26:19 +0200

    New changes3

--- install.sh
+++ install.sh
@@ -7,29 +7,16 @@ if [ ! -d "./models" ]; then
 fi
 
 # Definir la URL del modelo en Google Drive
-GDRIVE_MODEL_URL="https://drive.google.com/file/d/14ALOHcHxnE8mBLqmwLKJvxFVE-lK_2TZ/view?usp=drive_link"
+GDRIVE_MODEL_URL="https://drive.google.com/drive/folders/1VHEj-vU-f4qNhCKkYODlVQIfXkCRAuHs?usp=drive_link"
 
 # Descargar el modelo desde Google Drive a la carpeta ./models
 echo "Descargando el modelo desde Google Drive..."
-wget --no-check-certificate "$GDRIVE_MODEL_URL" -O ./models/model.zip
+wget --no-check-certificate "$GDRIVE_MODEL_URL" -O ./models/
 
 # Verificar si la descarga fue exitosa
 if [ $? -eq 0 ]; then
     echo "El modelo se descargó correctamente."
 
-    # Descomprimir el archivo descargado
-    echo "Descomprimiendo el archivo en ./models..."
-    unzip ./models/model.zip -d ./models
-    
-    # Verificar si la descompresión fue exitosa
-    if [ $? -eq 0 ]; then
-        echo "El archivo se descomprimió correctamente."
-        # Eliminar el archivo zip después de descomprimir
-        rm ./models/model.zip
-    else
-        echo "Hubo un problema al descomprimir el archivo."
-        exit 1
-    fi
 else
     echo "Hubo un problema al descargar el modelo."
     exit 1
